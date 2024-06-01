2a6cf73abe5640120edbc541f8f86da703723f20
Zied0023 <143819320+Zied0023@users.noreply.github.com>
2024-06-01 11:59:50 +0000

    LOGIN TEST

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'http://localhost',
+        'https://stunning-broccoli-jjp6x9jvxrfpx4g-3000.app.github.dev/api',
+      ]
+    }
+  }};
 
 module.exports = nextConfig;
